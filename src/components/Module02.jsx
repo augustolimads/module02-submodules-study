3d41de85c221ba9c85549111de384156d860c047
Augusto Lima <augustolimads@gmail.com>
2021-10-20 22:53:44 -0300

    add style mod

--- src/components/Module02.jsx
+++ src/components/Module02.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import style from "./style.module.css";
 
-export const Module02 = () => {
+export const Module02 = ({ test }) => {
   return (
-    <div>
+    <div className={style.card}>
       <h1>MODULE 02</h1>
+      {test}
     </div>
   );
 };
