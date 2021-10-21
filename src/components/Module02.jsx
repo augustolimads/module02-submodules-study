986a2a5ca0c2dde778cc48840bb99641e0934c6f
Augusto Lima <augustolimads@gmail.com>
2021-10-21 00:08:48 -0300

    add display

--- src/components/Module02.jsx
+++ src/components/Module02.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { Display } from "../../../module03-submodules-study/src/components/Display";
 import style from "./style.module.css";
 
 export const Module02 = ({ test }) => {
   return (
     <div className={style.card}>
       <h1>MODULE 02</h1>
+      <Display counter={666} />
       {test}
     </div>
   );
