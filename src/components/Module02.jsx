57f08acdbdc7cd76ca763791ab4efcf3face633b
Augusto Lima <augustolimads@gmail.com>
2021-11-03 14:23:12 -0300

    commiting all

--- src/components/Module02.jsx
+++ src/components/Module02.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import { Display } from "../../../module03-submodules-study/src/components/Display";
 import style from "./style.module.css";
 
-export const Module02 = ({ test }) => {
+export const Module02 = ({ counter, test }) => {
   return (
     <div className={style.card}>
       <h1>MODULE 02</h1>
-      <Display counter={1} />
+      <Display counter={counter} />
       {test}
     </div>
   );
